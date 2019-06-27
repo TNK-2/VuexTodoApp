fe57bb6134184a59b1cb0302234b1acee0609c59
ishizukayusuke <ishizukayusuke@team-lab.com>
2019-06-27 18:45:51 +0900

    ch7解説

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,48 @@
 <template>
   <div>
     <h2>タスク一覧</h2>
+    <ul>
+      <li v-for="task in tasks" v-bind:key="task.id">
+        <input type="checkbox" v-bind:checked="task.done" v-on:change="toggleTaskStatus(task)">
+        {{ task.name }}
+        -
+        <span v-for="id in task.labelIds" v-bind:key="id">
+          {{ getLabelText(id) }}
+        </span>
+      </li>
+    </ul>
+
+    <form v-on:submit.prevent="addTask">
+      <input type="text" v-model="newTaskName" placeholder="新しいタスク">
+    </form>
+
+    <h2>ラベル一覧</h2>
+    <ul>
+      <li v-for="label in labels" v-bind:key="label.id">
+        <input type="checkbox" v-bind:value="label.id" v-model="newTaskLabelIds">
+        {{ label.text }}
+      </li>
+    </ul>
+
+    <form v-on:submit.prevent="addLabel">
+      <input type="text" v-model="newLabelText" placeholder="新しいラベル">
+    </form>
+
+    <h2>ラベルでフィルター</h2>
+    <ul>
+      <li v-for="label in labels" v-bind:key="label.id">
+        <input type="radio" v-bind:checked="label.id === filter" v-on:change="changeFilter(label.id)">
+        {{ label.text }}
+      </li>
+      <li>
+        <input type="radio" v-bind:checked="filter == null" v-on:change="changeFilter(null)">
+        フィルターしない
+      </li>
+    </ul>
+
+    <h2>保存と復元</h2>
+    <button type="button" v-on:click="save">保存</button>
+    <button type="button" v-on:click="restore">復元</button>
   </div>
 </template>
 
@@ -8,13 +50,85 @@
 export default {
   data () {
     return {
+      // 入力中の新しいタスク名を一時的に保持する
+      newTaskName: '',
+
+      // 新しいタスクに紐づくラベル一覧を一時的に保持する
+      newTaskLabelIds: [],
+
+      // 入力中の新しいラベル名を一時的に保持する
+      newLabelText: ''
     }
   },
 
   computed: {
+    tasks () {
+      return this.$store.getters.filteredTasks
+    },
+
+    labels () {
+      return this.$store.state.labels
+    },
+
+    filter () {
+      return this.$store.state.filter
+    }
   },
 
   methods: {
+    // タスクを追加する
+    addTask () {
+      // `addTask` ミューテーションをコミット
+      this.$store.commit('addTask', {
+        name: this.newTaskName,
+        labelIds: this.newTaskLabelIds
+      })
+      this.newTaskName = ''
+      this.newTaskLabelIds = []
+    },
+
+    // タスクの完了状態を更新する
+    toggleTaskStatus (task) {
+      // `toggleTaskStatus` ミューテーションをコミット
+      this.$store.commit('toggleTaskStatus', {
+        id: task.id
+      })
+    },
+
+    // ラベルを追加する
+    addLabel () {
+      // `addLabel` ミューテーションをコミット
+      this.$store.commit('addLabel', {
+        text: this.newLabelText
+      })
+      this.newLabelText = ''
+    },
+
+    // ラベルの ID から、そのラベルのテキストを返す
+    getLabelText (id) {
+      const label = this.labels.filter(label => label.id === id)[0]
+      return label ? label.text : ''
+    },
+
+    // フィルターする対象のラベルを変更する
+    changeFilter (labelId) {
+      // `changeFilter` ミューテーションをコミット
+      this.$store.commit('changeFilter', {
+        filter: labelId
+      })
+    },
+
+    // 現在の状態を保存する
+    save () {
+      // `save` アクションをコミット
+      this.$store.dispatch('save')
+    },
+
+    // 保存されている状態を復元する
+    restore () {
+      // `restore` アクションをコミット
+      this.$store.dispatch('restore')
+    }
   }
 }
 </script>
--- src/store.js
+++ src/store.js
@@ -17,19 +17,124 @@ const store = new Vuex.Store({
       {
         id: 2,
         name: 'Vue の本を買う',
-        labelIds: [1, 3],
+        labelIds: [1, 3, 4],
         done: true
       }
-    ]
+    ],
+
+    // ラベルの初期ステート
+    labels: [
+      {
+        id: 1,
+        text: '買い物'
+      },
+      {
+        id: 2,
+        text: '食料'
+      },
+      {
+        id: 3,
+        text: '本'
+      },
+      {
+        id: 4,
+        text: 'Vue'
+      }
+    ],
+
+    // 次に追加するタスク、ラベルの ID
+    // 実際のアプリではサーバーで生成したり、UUID を使ったりする
+    nextTaskId: 3,
+    nextLabelId: 5,
+
+    // フィルターするラベルの ID
+    filter: null
   },
 
   getters: {
+    // フィルター後のタスクを返す
+    filteredTasks (state) {
+      // ラベルが選択されていなければそのままの一覧を返す
+      if (!state.filter) {
+        return state.tasks
+      }
+
+      // 選択されているラベルでフィルタリングする
+      return state.tasks.filter(task => {
+        return task.labelIds.indexOf(state.filter) >= 0
+      })
+    }
   },
 
   mutations: {
+    // タスクを追加する
+    addTask (state, { name, labelIds }) {
+      state.tasks.push({
+        id: state.nextTaskId,
+        name,
+        labelIds,
+        done: false
+      })
+
+      // 次に追加されるタスクに付与する ID を更新する
+      state.nextTaskId++
+    },
+
+    // タスクの完了状態を変更する
+    toggleTaskStatus (state, { id }) {
+      const filtered = state.tasks.filter(task => {
+        return task.id === id
+      })
+
+      filtered.forEach(task => {
+        task.done = !task.done
+      })
+    },
+
+    // ラベルを追加する
+    addLabel (state, { text }) {
+      state.labels.push({
+        id: state.nextLabelId,
+        text
+      })
+
+      // 次に追加されるラベルに付与する ID を更新する
+      state.nextLabelId++
+    },
+
+    // フィルタリング対象のラベルを変更する
+    changeFilter (state, { filter }) {
+      state.filter = filter
+    },
+
+    // ステートを復元する
+    restore (state, { tasks, labels, nextTaskId, nextLabelId }) {
+      state.tasks = tasks
+      state.labels = labels
+      state.nextTaskId = nextTaskId
+      state.nextLabelId = nextLabelId
+    }
   },
 
   actions: {
+    // ローカルストレージにステートを保存する
+    save ({ state }) {
+      const data = {
+        tasks: state.tasks,
+        labels: state.labels,
+        nextTaskId: state.nextTaskId,
+        nextLabelId: state.nextLabelId
+      }
+      localStorage.setItem('task-app-data', JSON.stringify(data))
+    },
+
+    // ローカルストレージからステートを復元する
+    restore ({ commit }) {
+      const data = localStorage.getItem('task-app-data')
+      if (data) {
+        commit('restore', JSON.parse(data))
+      }
+    }
   }
 })
 
